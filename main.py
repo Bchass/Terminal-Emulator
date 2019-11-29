0d79c215d392326f4de42a91a440962aef878be0
Brandon Chasser <brandonchasser22@gmail.com>
2019-11-29 18:27:38 -0500

    some commands working

--- main.py
+++ main.py
@@ -9,8 +9,27 @@ class Terminal(QtWidgets.QMainWindow):
     def __init__(self):
         super(Terminal, self).__init__()
         uic.loadUi('gui.ui', self)
+        self.lineEdit.returnPressed.connect(self.Drake)
+        self.working_directory = "."
+
+# Drake is just a placeholder for now, till I figure out a better name
+    def commands(self):
+        commands = self.lineEdit.text()
+        
+        if "cd" in commands:
+            values = commands.split(" ")
+            if values[1][0] == "/":
+                self.working_directory = values[1]
+            else:
+                self.working_directory = self.working_directory + "/" + values[1]
+        
+        print(self.working_directory)
+
+        subprocess.call(commands, shell=True, cwd=self.working_directory)
+        
        
-    
+
+
 app = QtWidgets.QApplication([])
 gui = Terminal()
 gui.show()
