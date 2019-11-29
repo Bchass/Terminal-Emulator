92a4fd0aa378bd153f4fdec91fe29cc08601275f
Brandon Chasser <brandonchasser22@gmail.com>
2019-11-29 15:16:16 -0500

    display ui

--- main.py
+++ main.py
@@ -3,16 +3,15 @@ from PyQt5.QtWidgets import QMessageBox, QDialog, QFileDialog
 import sys
 import os
 import subprocess
-
+ 
 class Terminal(QtWidgets.QMainWindow):
 
     def __init__(self):
         super(Terminal, self).__init__()
         uic.loadUi('gui.ui', self)
-        self.title = 'Drake'
-
-if __name__ == '__main__':
-    app = QtWidgets.QApplication([])
-    gui = Terminal()
-    gui.show
-    sys.exit(app.exec_())
+       
+    
+app = QtWidgets.QApplication([])
+gui = Terminal()
+gui.show()
+sys.exit(app.exec())
