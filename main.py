94b3f87414fa5c109c2e11ac892ec968faa4fbad
Brandon Chasser <brandonchasser22@gmail.com>
2019-11-29 13:49:11 -0500

    Setup for gui.ui

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ import sys
 import os
 import subprocess
 
+class Terminal(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(Terminal, self).__init__()
+        uic.loadUi('gui.ui', self)
+        self.title = 'Drake'
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    gui = Terminal()
+    gui.show
+    sys.exit(app.exec_())
