451166fee4961f9775e9dcc57f1f864e08862864
Brandon Chasser <brandonchasser22@gmail.com>
2019-12-12 23:58:40 -0500

    Added function to take care of useless errors

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import QMessageBox, QDialog, QFileDialog
 import sys
 import os
 import subprocess as sp
- 
+
 class Terminal(QtWidgets.QMainWindow):
 
     def __init__(self):
@@ -12,18 +12,16 @@ class Terminal(QtWidgets.QMainWindow):
         self.lineEdit.returnPressed.connect(self.commands)
         self.working_directory = "."
 
-
     def commands(self):
         commands = self.lineEdit.text()
         os.system('clear') #clear in shell
-
+    
         if "cd" in commands:
             values = commands.split(" ")
-            if values[1] == "/":
+            if values[1][0] == "/":
                 self.working_directory = values[1]
             else:
                 self.working_directory = self.working_directory + "/" + values[1]
-
     
         print(self.working_directory)
 
@@ -31,9 +29,16 @@ class Terminal(QtWidgets.QMainWindow):
 
         self.textBrowser.setText(self.textBrowser.toPlainText() + "\n➜" + commands) #show input in GUI
 
-        output = sp.check_output(commands,shell=True, cwd=self.working_directory) #show output in GUI
-        self.textBrowser.setText(self.textBrowser.toPlainText() + "\n" + output.decode('UTF-8'))
-
+    def error(self):
+        try:
+            output = sp.check_output(commands,shell=True, cwd=self.working_directory) #show output in GUI
+            self.textBrowser.setText(self.textBrowser.toPlainText() + "\n" + output.decode('UTF-8'))
+        except Exception:
+            output = str(output)
+        finished = output.split('\n')
+        for line in finished:
+            print(line)
+        return
 
 app = QtWidgets.QApplication([])
 gui = Terminal()
